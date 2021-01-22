bee47532efe46641a0297e76f2911922a129c3b8
ocawthorne <ocawthorne@gmail.com>
2021-01-22 14:01:58 +0000

    commit tree f3d5a048f45e3ab3fda56d87602867a1cc4864fa

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+function map(array, callback) {
+   let newArray = []
+   array.forEach(element => newArray.push(callback(element)))
+   return newArray
+}
+
+function reduce(array, callback, start=0) {
+   let memo = start
+   array.forEach(element => start += callback(element, memo))
+   return start + memo
+}
